0689f766391847815b15bf9ae38e68f4ed6e736d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-27 12:15:20 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                 name: "Sales & Promos",
                 id: "stores",
                 href: "/promotions",
-                parent_id: "stores_link",
+                parent_id: "sales_link",
                 parent_class_list: "menu_item",
                 hover_id: "sales_underline",
                 hover_class_list:"menu_underline"
