3b4b633927a9500fd5ba3763ef5b56bd118e0a4e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-27 12:17:43 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                 href: "/",
                 parent_id: "hours",
                 parent_class_list: "menu_item",
-                hover_id: "events_underline",
+                hover_id: "hours_underline",
                 hover_class_list:"menu_underline"
             }
         ],
