3a31a856defe382bb8d31aea16f29cea79e7ba51
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-27 12:52:10 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -130,7 +130,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
         // console.log(this.$store.getters.getTodayHours);
       },
       toggle_sitemap: function () {
-          console.log(toggle_sitemap);
+          console.log(this.toggle_sitemap);
       }
     },
     computed: {
