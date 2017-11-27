0049f7155e1a84766f0887c3c30036d63141d051
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-27 16:53:18 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -1,31 +1,130 @@
 <template>
   <div class="row page_container">
-    <div class="large-6 columns">
-      <div>
-        <h1>{{title}}</h1>
-        <p>{{description}}</p>
-        <hr/>
-        <h5>Translated text example:</h5>
-        <p>{{ $t("message.hello") }}</p>
-        <p>{{property.name}}</p>
-        <hr/>
-        <today-hours></today-hours>
-        <search-component v-model="search" :list="processedStores" :suggestion-attribute="suggestionAttribute" @select="onOptionSelect">
-          <template slot="item" scope="option">
-            <article class="media">
-              <!--<figure class="media-left">
-                <p class="image is-64x64">
-                  <img :src="option.data.store_front_url_abs">
-                </p>
-              </figure>-->
-              <p>
-                <strong>{{ option.data.name }}</strong>
-              </p>
-            </article>
-          </template>
-        </search-component>
-      </div>
+    <meta name="google-site-verification" content="r4VQ6mOTPaoCYBAa2NhA7zXrL97iqN02tOEmsQpSQkU" />
+<style>
+    .flexslider, .slides img{
+    display:inline-block;
+    width:708px;
+    height:458px;
+}
+
+.flex-control-nav{
+    bottom:5px;
+    
+}
+
+.flex-control-paging li a{
+    border-radius:50%;
+    height:15px;
+    width:15px;
+}
+
+
+
+.banner-description{
+    display:none;
+}
+
+
+@media (max-width: 767px) {
+    .flexslider, .slides img {
+        display: inline-block;
+        width: 100%;
+        height:auto;
+    }
+    .flex-control-nav{
+        right:0;
+}
+</style>
+
+
+<div class="banner_div">
+
+    
+    <div class="flexslider">
+        <ul class="slides"> 
+            {% assign counter = 1 %}
+            {% for banner_image in banner_images %}
+                <li id="{{counter}}" style="display:none">
+                    {% if banner_image.url != "" %}
+                        <a href="{{banner_image.url}}" class="flex-image">
+                            <img src="{{banner_image.image_url}}" data-pin-no-hover="true"/>
+                          
+                        </a>
+                    {% else %}
+                        <img src="{{banner_image.image_url}}" data-pin-no-hover="true" class="flex-image"/>
+                       
+                    {% endif %}
+                    <p class="banner-description">{{banner_image.description}}</p>
+                {% assign counter = counter | plus:1 %}
+                    
+                </li>
+            {% endfor %}
+        </ul>
     </div>
+    
+    
+    <div class="top_two hidden-phone">
+        {% for item in site.feature_items_array %}
+            {% if item.name == 'top_1' %}
+                <a href='{{item.url}}' ><img class="top_image" src='{{item.feature_image_path}}'/></a>
+            {% endif %}
+            {% if item.name == 'top_2' %}
+                <a href='{{item.url}}' ><img class="top_image" src='{{item.feature_image_path}}'/></a>
+            {% endif %}
+        {% endfor %}
+    </div>
+</div>
+
+<div style="border-top:4px double #aea99e">
+    <div class="feature-div" style="height:580px">
+        <span>
+            {% for item in site.feature_items_array limit:3 %}
+                <div class="feature-box">
+                    <p class="feature-title">{{item.name}}</p>
+                    <img src="{{item.feature_image_path}}" class="feature-image" />
+                    <p class="feature-html">{{item.html}}</p>
+                    <p class="feature-desc">{{item.description | truncate: 100}}</p>
+                    <span><a href="{{item.url}}" class="mobile_readmore" ><p class="feature-readmore">Read More</p><img src={{3573 | get_image_url}} class="pull-left"/></a></span>
+                    {% if @can_edit == "true" %}
+                        <a href="/feature_items/{{item.id}}/edit" class="btn btn-primary pull-right">Edit</a>
+                    {% endif %} 
+                </div>
+            {% endfor %}
+        </span>
+    </div>
+</div>
+<script>
+    $("#home_link").css("color","black");
+    $(".feature-box:nth-child(-n+2)").css("margin-right","45px");
+    $(document).ready(function() {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            controlNav: true,
+            directionNav: true,        
+            prevText: "Previous",
+            nextText: "Next",
+            start: function(){
+                 $('.slides li').css("display","block"); 
+            },
+        });
+      
+    });
+    
+    $(".swipe_test").on("swipe",function(){
+      alert("success");
+    });
+    
+    $( ".slides li" ).hover(
+      function() {
+        $(this.id+"-pin").show();
+      }, function() {
+        $(this.id+"-pin").hide();
+      }
+    );
+    
+</script>
+
   </div>
 </template>
 
