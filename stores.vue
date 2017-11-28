2472e3c87b6e0bb1ee319731337dbb97edb0f7a8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 17:35:15 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -446,7 +446,7 @@
 </style>
 
 <script>
-    define(["Vue",'vue-select'], function(Vue, v-select) {
+    define(["Vue",'vue-select'], function(Vue, VueSelect) {
         return Vue.component("stores-component", {
             template: template, // the variable template will be injected
             data: function() {
