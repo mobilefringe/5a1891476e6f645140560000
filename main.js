afec2d08b141b54a24186553650efce7c781c60d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 17:27:23 -0500

    Canyon Crest 1.0  /main.js

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ require.config({
     'today_hours': 'https://mmvue.codecloudapp.com/today_hours.vue?noext', //append a dummy query string so requireJS doesn't auto-append .js to the end of the url
     'search-component': 'https://mmvue.codecloudapp.com/search-component.vue?noext',
     'slick' : 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min',
-    'vue-slick' : 'https://mmvue.codecloudapp.com/slick.vue?noext'
+    'vue-slick' : 'https://mmvue.codecloudapp.com/slick.vue?noext',
+    'vue-select' : 'https://cdn.jsdelivr.net/npm/vue-select@2.3.0/dist/vue-select.min'
   }
 });
 
