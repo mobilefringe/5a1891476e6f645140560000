150a4eb2ce90806e71d3ada7cb9b201dd31d0fdf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 14:14:05 -0500

    Canyon Crest 1.0  /slick.vue

---
+++ slick.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
+    return Vue.component("today-hours", {
+      template: template, // the variable template will be injected,
+      data: function() {
+        return {
+          todayDate: new Date(),
+          description: "An example of integration of Mall Maverick with Vue.js"
+        }
+      },
+      computed: {
+        hours () {
+          return this.$store.getters.getTodayHours;
+        },
+        timezone () {
+          return this.$store.getters.getTimezone;
+        }
+      }
+    });
+  });
+</script>
