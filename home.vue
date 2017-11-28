cc9c969861d0a4969adfc38f53c371bc170eb16b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 15:09:17 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -18,6 +18,7 @@
             </div>
             
             <div class="top_two hidden-phone">
+                
                 <!--{% for item in site.feature_items_array %}-->
                 <!--    {% if item.name == 'top_1' %}-->
                 <!--        <a href='{{item.url}}' ><img class="top_image" src='{{item.feature_image_path}}'/></a>-->
