aaeb45ece01b11c4a82febb4623515f8b43e9ab7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 12:12:09 -0500

    Canyon Crest 1.0  /slider.vue

--- slider.vue
+++ slider.vue
@@ -7,25 +7,12 @@
 </template>
 
 <script>
-  define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
-    return Vue.component("today-hours", {
-      template: template, // the variable template will be injected,
-      data: function() {
-        return {
-          todayDate: new Date(),
-          description: "An example of integration of Mall Maverick with Vue.js"
-        }
-      },
-      computed: {
-        hours () {
-          return this.$store.getters.getTodayHours;
-        },
-        timezone () {
-          return this.$store.getters.getTimezone;
-        }
-      }
+    define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
+        return Vue.component("today-hours", {
+            template: template, // the variable template will be injected,
+      
+        });
     });
-  });
 </script>
 
 <style lang="scss" type="text/scss">
