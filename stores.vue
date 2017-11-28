d72abace160979835cf5f53f192f7728a640cfcf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 17:29:23 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -105,35 +105,35 @@
         </div>
         
         <div class="alphabet-dd visible-phone">
-            <select onchange="window.location.href='#'+this.value">
-                <option value="" disabled selected>Select Store A-Z</option>
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-                <option value="D">D</option>
-                <option value="E">E</option>
-                <option value="F">F</option>
-                <option value="G">G</option>
-                <option value="H">H</option>
-                <option value="I">I</option>
-                <option value="J">J</option>
-                <option value="K">K</option>
-                <option value="L">L</option>
-                <option value="M">M</option>
-                <option value="N">N</option>
-                <option value="O">O</option>
-                <option value="P">P</option>
-                <option value="Q">Q</option>
-                <option value="R">R</option>
-                <option value="S">S</option>
-                <option value="T">T</option>
-                <option value="U">U</option>
-                <option value="V">V</option>
-                <option value="W">W</option>
-                <option value="X">X</option>
-                <option value="Y">Y</option>
-                <option value="Z">Z</option>
-            </select>
+            <!--<select onchange="window.location.href='#'+this.value">-->
+            <!--    <option value="" disabled selected>Select Store A-Z</option>-->
+            <!--    <option value="A">A</option>-->
+            <!--    <option value="B">B</option>-->
+            <!--    <option value="C">C</option>-->
+            <!--    <option value="D">D</option>-->
+            <!--    <option value="E">E</option>-->
+            <!--    <option value="F">F</option>-->
+            <!--    <option value="G">G</option>-->
+            <!--    <option value="H">H</option>-->
+            <!--    <option value="I">I</option>-->
+            <!--    <option value="J">J</option>-->
+            <!--    <option value="K">K</option>-->
+            <!--    <option value="L">L</option>-->
+            <!--    <option value="M">M</option>-->
+            <!--    <option value="N">N</option>-->
+            <!--    <option value="O">O</option>-->
+            <!--    <option value="P">P</option>-->
+            <!--    <option value="Q">Q</option>-->
+            <!--    <option value="R">R</option>-->
+            <!--    <option value="S">S</option>-->
+            <!--    <option value="T">T</option>-->
+            <!--    <option value="U">U</option>-->
+            <!--    <option value="V">V</option>-->
+            <!--    <option value="W">W</option>-->
+            <!--    <option value="X">X</option>-->
+            <!--    <option value="Y">Y</option>-->
+            <!--    <option value="Z">Z</option>-->
+            <!--</select>-->
         </div>
         <div class="mobile_download_dd visible-phone">
             <div class="dropdown">
