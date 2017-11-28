67170d6d26b46659529a663e31ad2ab25fa79327
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 15:10:41 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -19,7 +19,7 @@
             
             <div class="top_two hidden-phone">
                 <router-link v-for="feature in feature_items">
-                    <img class="top_image" src='{{item.feature_image_path}}'/>
+                    <img class="top_image" src='{{feature.feature_image_path}}'/>
                 </router-link>
                 <!--{% for item in site.feature_items_array %}-->
                 <!--    {% if item.name == 'top_1' %}-->
