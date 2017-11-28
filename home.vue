9d5ea6179d0ffa6ff01497522de71d904b7bbf2d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 14:44:03 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -24,8 +24,8 @@
             <!--    </div>-->
             <!--</agile>-->
             
-            <slick ref="slick" :options="slickOptions">
             <div  class="prev"></div>
+            <slick ref="slick" :options="slickOptions">
                 <div class="" v-for="banner in banners">
                     <img :src="banner.image_url" class="" alt="">
                     <div class="banner_text">
@@ -36,8 +36,9 @@
                     </div>
                     
                 </div>
-                <div class="next"></div>
+                
             </slick>
+            <div class="next"></div>
             <div class="top_two hidden-phone">
                 <!--{% for item in site.feature_items_array %}-->
                 <!--    {% if item.name == 'top_1' %}-->
