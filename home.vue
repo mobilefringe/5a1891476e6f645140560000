4013ea22791e3c8bf96beedd2a4cb42d5216e105
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 15:18:25 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -19,7 +19,7 @@
             
             <div class="top_two hidden-phone">
                 <div class="banner_features">
-                    <router-link v-for="feature in banner_features" :to="feature.url">
+                    <router-link :to="feature.url">
                         <img class="top_image" :src='feature.image_url'/>
                         <div class="feature_overlay">{{feature.name}}</div>
                     </router-link>
