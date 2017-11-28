a8e11b7b7459db86dded833dfa939c70b635cf67
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 15:15:42 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -90,7 +90,7 @@
                 },
                 feature_items () {
                     console.log(this.$store.state.results.feature_items);
-                    return _.slice(this.$store.state.results.feature_items, 2);
+                    return _.slice(this.$store.state.results.feature_items, 2,this.$store.state.results.feature_items.length );
                 }
             },
             methods: {
