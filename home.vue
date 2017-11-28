920361b2672e0fe1b738fabff97ee58cd74b487b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 13:10:29 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -11,7 +11,7 @@
             <!--        </li>-->
             <!--    </ul>-->
             <!--</div>-->
-            <agile :arrows="false" :dots="false" :infinite="false">
+            <agile :arrows="false" :dots="true" :infinite="false">
                 <div class="slide slide--1"><h3>slide 1</h3></div>
                 <div class="slide slide--2"><h3>slide 2</h3></div>
                 <div class="slide slide--3"><h3>slide 3</h3></div>
