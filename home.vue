b755d52906f3d4d2b86c00e2ac3e222dab7f97cf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 12:20:30 -0500

    Canyon Crest 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -12,13 +12,13 @@
             <!--        </li>-->
             <!--    </ul>-->
             <!--</div>-->
-            <agile :infinite="false">
-                <div class="slide slide--1"><h3>slide 1</h3></div>
-                <div class="slide slide--2"><h3>slide 2</h3></div>
-                <div class="slide slide--3"><h3>slide 3</h3></div>
-                <div class="slide slide--4"><h3>slide 4</h3></div>
-                <div class="slide slide--5"><h3>slide 5</h3></div>
-                <div class="slide slide--6"><h3>slide 6</h3></div>
+            <agile>
+                <div class="slide slide--1"> <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a></div>
+                <div class="slide slide--2"> <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a></div>
+                <div class="slide slide--3"> <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a></div>
+                <div class="slide slide--4"> <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a></div>
+                <div class="slide slide--5"> <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a></div>
+                <div class="slide slide--6"> <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a></div>
             </agile>
             <div class="top_two hidden-phone">
                 <!--{% for item in site.feature_items_array %}-->
@@ -106,77 +106,3 @@
         })
     })
 </script>
-
-<style>
-    .agile {
-        &__dots {
-            bottom: 0;
-            left: 50%;
-            position: absolute;
-            transform: translateX(-50%);
-        }
-    
-        &__dot {
-            button {
-                background-color: transparent;
-                border: 1px solid #fff;
-    
-                &:hover {
-                    background-color: #fff;
-                }
-            }
-    
-            &--current {
-                button {
-                    background-color: #fff;
-                }
-            }
-        }
-    
-        &__arrow {
-            height: 100%;
-            top: 0;
-            width: 80px;
-    
-            &:hover {
-                background-color: rgba(#000, .5);
-    
-                #arrow-svg {
-                    fill: #fff;
-                }
-            }
-    
-            &[disabled] {
-                display: none;
-            }
-    
-            #arrow-svg {
-                fill: rgba(#fff, .4);
-                height: 25px;
-            }
-        }
-    }
-    
-    .slide {
-        background: {
-            position: center;
-            size: cover;
-        }
-        height: 500px;
-    
-        &:before {
-            background-color: rgba(#000, .2);
-            content: '';
-            height: 100%;
-            left: 0;
-            position: absolute;
-            top: 0;
-            width: 100%;
-        }
-    
-        &--1 {
-            background-image: url(../img/bg-1_1.jpg);
-        }
-    
-    }
-</style>
