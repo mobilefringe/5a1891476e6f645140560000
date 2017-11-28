c5c2abf9d081ed6da01f32f9a2eed85c0159e4fd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-28 12:40:27 -0500

    Canyon Crest 1.0  /agile.vue

--- agile.vue
+++ agile.vue
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="//preview-5a1891476e6f645140560000.codecloudapp.com/agile.css">
 <template>
     <div class="agile"
          :class="{'agile--fade': settings.fade && !settings.unagile, 'agile--disabled': settings.unagile}">
