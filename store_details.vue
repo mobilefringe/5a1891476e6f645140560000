8b4de551bab029daff98be7f5ee66971b381d578
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 14:21:19 -0500

    Canyon Crest 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -5,8 +5,6 @@
         <h1>{{currentStore.name}}</h1>
         <p>{{currentStore.description}}</p>
         <a v-bind:href="currentStore.website">{{currentStore.website}}</a>
-        
-        <a v-bind:href="currentStore.website">{{currentStore.website}}</a>
       </div>
     </div>
     <div class="col-md-8">
