e69c75f25bf6f748dc53cca0ec2b416df63ecc87
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 10:50:20 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -28,44 +28,6 @@
                 <div id="mapsvg_store_detail"></div>
             </div>
         </div>
-    <!--<ul class="menu">-->
-    <!--  <li><a v-on:click="changeMode('alphabetical')">Alphabetical</a></li>-->
-    <!--  <li><a v-on:click="changeMode('category')">Category</a></li>-->
-    <!--</ul>-->
-    <!--<hr/>-->
-    <!--<div class="columns large-12" v-for="(stores, index) in storesByAlphaIndex" v-if="listMode === 'alphabetical'">-->
-    <!--  <div class="list_header">-->
-    <!--    <b>{{index}}</b>-->
-    <!--    <hr/>-->
-    <!--  </div>-->
-    <!--  <div class="store-section" v-for="store in stores">-->
-    <!--    <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>-->
-    <!--    <hr/>-->
-    <!--  </div>-->
-      <!-- <div class="card">
-        <div class="card-divider">
-          {{ store.name }}
-        </div>
-        <div class="card-section center">
-          <a :href="store.image_url" target="_blank"><img :src="store.image_url"></a>
-        </div>
-        <div class="card-section">
-          <div class="center">
-            <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">View Details</router-link>
-          </div>
-        </div>
-      </div> -->
-    <!--</div>-->
-    <!--<div class="columns large-12" v-for="(stores, index) in storesByCategoryName" v-if="listMode === 'category'">-->
-    <!--  <div class="list_header">-->
-    <!--    <b>{{index}}</b>-->
-    <!--    <hr/>-->
-    <!--  </div>-->
-    <!--  <div class="store-section" v-for="store in stores">-->
-    <!--    <router-link :to="{ name: 'storeDetails', params: { id: store.slug }}">{{store.name}}</router-link>-->
-    <!--    <hr/>-->
-    <!--  </div>-->
-    <!--</div>-->
   </div>
 </template>
 
