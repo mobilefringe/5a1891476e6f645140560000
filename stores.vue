55463370f646c4acfbc10594a84ce64e304df7e7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 13:40:31 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -1,43 +1,45 @@
 <template>
-    <div class="page_header all_caps double_border_bottom">
-        <div class="page_container"> Stores Directory & Map </div>
-    </div>
-    <div class="page_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        
+    <div>
         <div class="page_header all_caps double_border_bottom">
-            Stores Directory & Map
+            <div class="page_container"> Stores Directory & Map </div>
         </div>
-        <div class="store-list-left-block col-md-4 padding_top_20">
-            <div class="store-list-container">
-                <div class="dropdown_container hidden-phone">
-                    <p class="text_left">Select Category</p>
-                    <div class="category-DD-div">
-                        <v-select v-model="selected_cat" :options="dropDownCats" :searchable="false" class="open" on-change="consoleCallback"></v-select>
+        <div class="page_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+            
+            <div class="page_header all_caps double_border_bottom">
+                Stores Directory & Map
+            </div>
+            <div class="store-list-left-block col-md-4 padding_top_20">
+                <div class="store-list-container">
+                    <div class="dropdown_container hidden-phone">
+                        <p class="text_left">Select Category</p>
+                        <div class="category-DD-div">
+                            <v-select v-model="selected_cat" :options="dropDownCats" :searchable="false" class="open" on-change="consoleCallback"></v-select>
+                        </div>
+                    </div>
+                    <div class="alphabet-dd visible-phone">
+                        <v-select v-model="selected_cat" :options="dropDownCats"></v-select>
+                    </div>
+                    
+                    <div class="alphabet-dd visible-phone">
+                        <v-select v-model="selected_alpha" :options="alphabet"></v-select>
+                    </div>
+              
+                    <ul class="store-listing text_left  padding_top_20">
+                        <li v-for="store in allStores">
+                            <a :href="'/stores/'+store.slug"><p class="directory_store_name">{{store.name}}</p></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="store-list-map-block col-md-8">
+                <div class="main-map">
+                    <div id="mapsvg_store_detail">
+                       <img src="http://placehold.it/2000x1000" alt="">
                     </div>
                 </div>
-                <div class="alphabet-dd visible-phone">
-                    <v-select v-model="selected_cat" :options="dropDownCats"></v-select>
-                </div>
-                
-                <div class="alphabet-dd visible-phone">
-                    <v-select v-model="selected_alpha" :options="alphabet"></v-select>
-                </div>
-          
-                <ul class="store-listing text_left  padding_top_20">
-                    <li v-for="store in allStores">
-                        <a :href="'/stores/'+store.slug"><p class="directory_store_name">{{store.name}}</p></a>
-                    </li>
-                </ul>
             </div>
-        </div>
-        <div class="store-list-map-block col-md-8">
-            <div class="main-map">
-                <div id="mapsvg_store_detail">
-                   <img src="http://placehold.it/2000x1000" alt="">
-                </div>
-            </div>
-        </div>
-  </div>
+      </div>
+      </div>
 </template>
 
 <style>
