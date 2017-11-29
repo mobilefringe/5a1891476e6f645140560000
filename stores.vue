1f4a3cdc1c9d467e0dcc6c9378e7f254717b1a0e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 10:48:00 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -19,7 +19,7 @@
                 <ul class="store-listing">
                         <li class="directory_header">#</li>
                         
-                        <li v-for="store in all>
+                        <li v-for="store in allStores">
                         <a href="/stores/northpark-7-for-all-mankind"><p class="directory_store_name">7 For All Mankind</p></a>
                         
                          
