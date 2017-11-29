a1fa0369d0866e1af365cdb0be71ac0e41c7fb95
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 13:38:05 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <div class="page_header">
-            
+            Stores Directory & Map
         </div>
         <div class="store-list-left-block col-md-4 padding_top_20">
             <div class="store-list-container">
