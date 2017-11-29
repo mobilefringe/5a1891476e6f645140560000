178b78dbe2f2b479dd6e584288e4b1cffc70e596
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 14:04:58 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -68,7 +68,7 @@
             data: function() {
                 return {
                   listMode: "alphabetical",
-                  selected_cat : null,
+                  selected_cat : "All",
                   selected_alpha : null,
                   alphabet : ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
                 }
