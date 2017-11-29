8d5c3784939f613fb9a222092f48d31ace4e67aa
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-11-29 13:39:01 -0500

    Canyon Crest 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -1,6 +1,12 @@
 <template>
+<div class="page_header all_caps double_border_bottom">
+            Stores Directory & Map
+        </div>
     <div class="page_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
         
+        <div class="page_header all_caps double_border_bottom">
+            Stores Directory & Map
+        </div>
         <div class="store-list-left-block col-md-4 padding_top_20">
             <div class="store-list-container">
                 <div class="dropdown_container hidden-phone">
